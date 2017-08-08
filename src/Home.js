f842d4ed28d1f7560986efdd45860f65fa4f54d0
Fraser Price <fraser@Frasers-MacBook-Air.local>
2017-08-08 19:16:03 +0100

    Added selection for visualisation. Now makes call to online api: https://chromium-db-app.herokuapp.com/

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Chart } from 'react-google-charts'
 import fetch from 'isomorphic-fetch'
 
-const URL = 'http://localhost:3000/'
+const URL = 'https://chromium-db-app.herokuapp.com/';
 
 class Home extends React.Component {
   constructor() {
@@ -16,7 +16,7 @@ class Home extends React.Component {
   getTreemap = (isPdfium, allBugs, query, callback) => {
     const platform = isPdfium ? 'pdfium/' : 'chromium/';
     const treemap = allBugs ? 'treemap/' : 'security_treemap/';
-    const fullUrl = URL + platform + treemap + query
+    const fullUrl = URL + platform + treemap + query;
     fetch(fullUrl, {
       method: 'GET',
       headers: {
@@ -26,6 +26,51 @@ class Home extends React.Component {
     }).then(response => response.json()
     ).then(json => callback(false, json)
     ).catch(err => callback(err));
+  };
+
+  onChangeTreemap = () => {
+    const e = document.getElementById("select-treemap");
+    const chartValue = e.options[e.selectedIndex].value;
+    let isPdfium, allBugs;
+    switch(chartValue) {
+      case 'pdfium-all':
+        isPdfium = true;
+        allBugs = true;
+        break;
+      case 'pdfium-security':
+        isPdfium = true;
+        allBugs = false;
+        break;
+      case 'pdfium-all-vs-security':
+        isPdfium = true;
+        allBugs = true;
+        break;
+      case 'chromium-all':
+        isPdfium = false;
+        allBugs = true;
+        break;
+      case 'chromium-security':
+        isPdfium = false;
+        allBugs = false;
+        break;
+      default:
+        isPdfium = true;
+        allBugs = true;
+    }
+    this.getTreemap(isPdfium, allBugs, 'status:closed', (err, json) => {
+      if(err) {
+        alert(err);
+      } else {
+        json.map(data => {
+          if(!isNaN(parseInt(data[2]))) {
+            data[2] = parseInt(data[2]);
+            data[3] = parseInt(data[3]);
+          }
+          return data
+        });
+        this.setState({treemapLoaded: true, treemap: json});
+      }
+    });
   };
 
   render() {
@@ -42,7 +87,6 @@ class Home extends React.Component {
             }
             return data
           });
-          alert(JSON.stringify(json));
           this.setState({treemapLoaded: true, treemap: json});
         }
       });
@@ -50,7 +94,21 @@ class Home extends React.Component {
 
     return (
       <div>
-        <p>Home!</p>
+        <h1>Visualisations</h1>
+        <p>
+          This is a tool for viewing an overview of bugs in the Chromium and Pdfium projects as they appear in the
+          source code. API calls can be made
+          <a href={URL}> here.</a>
+
+        </p>
+        <p>Select which visualisation you would like to view:</p>
+        <select id="select-treemap" onChange={this.onChangeTreemap}>
+          <option value="pdfium-all">P: All</option>
+          <option value="pdfium-security">P: Security</option>
+          <option value="pdfium-all-vs-security">P: All : Security</option>
+          <option value="chromium-all">C: All</option>
+          <option value="chromium-security">C: Security</option>
+        </select>
         <Chart
           chartType="TreeMap"
           data={this.state.treemap}
